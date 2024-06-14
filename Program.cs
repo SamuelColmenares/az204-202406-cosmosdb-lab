b42a6cbcfce81807bfe012a83c0bf9bea6ba29f6
Samuel Colmenares <samuelcolmenares@MacBook-Pro-de-Samuel.local>
2024-06-13 21:34:06 -0500

    codigo inicial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos;
+
+string EndpointUri = "<<Endpoint-general>>";
+string PrimaryKey = "<<Primary-key>>";
+string databaseId = "az204Database";
+string containnerId ="az204Container";
+
+try
+{
+    Console.WriteLine("Inicia app cosmos db...");
+    CosmosClient cosmosClient= new CosmosClient(EndpointUri,PrimaryKey);
+    var database = await cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+    Console.WriteLine($"BD creada: {database.Database.Id}");
+    var container = await database.Database.CreateContainerIfNotExistsAsync(containnerId, "/LastName");
+    Console.WriteLine($"Container creado: {container.Container.Id}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("", ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finaliza el programa... ");
+    Console.ReadKey();
+}
+
+
